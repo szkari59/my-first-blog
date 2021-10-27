6645577890e01baa1fefabc62be44531eace042f
kari <karoly.szenasu@gmail.com>
2021-10-27 16:56:12 +0200

    Most talán jó

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-     re_path(r'^$', views.post_list, name='post_list'),
-     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
  ]
